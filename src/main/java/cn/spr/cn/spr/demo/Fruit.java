75448faf0fd2d4e1ac3e8ad7f79476a1350bd161
WXQD <1452478328@qq.com>
2017-04-09 17:20:48 +0800

    新增一个类

---
+++ src/main/java/cn/spr/cn/spr/demo/Fruit.java
@@ -0,0 +1,24 @@
+package cn.spr.cn.spr.demo;
+
+/**
+ * Created by Administrator on 2017-04-09.
+ */
+public class Fruit {
+    private String name;
+    private String price;
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
